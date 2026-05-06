dfa8d862b2bd79e5c43b2b85f5591153bbec92d9
Fernando Polanco <A24216406@alumnos.uady.mx>
2026-05-06 17:49:10 -0600

    Empezamos de nuevo

--- src/main/java/mx/uady/simulador/models/Scheduler.java
+++ src/main/java/mx/uady/simulador/models/Scheduler.java
@@ -1,10 +1,5 @@
 package mx.uady.simulador.models;
 
-
-/**
- * Contiene la lógica del algoritmo 3: SRTF (Shortest Remaining Time First).<p>
- * Es un algoritmo preemptive: en cada unidad de tiempo, la CPU se asigna al proceso con el menor tiempo restante de ráfaga.
- */
 public class Scheduler {
-    private final static double CONTEXT_CHANGE_TIME = 0.2;
+
 }
